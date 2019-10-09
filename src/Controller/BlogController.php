40afe5eafa6037da27a0c9ac0e49048afc4c3eb9
Oscar Diaz <o-pipe@hotmail.com>
2019-10-09 00:35:59 -0500

    Crea estructura de archivos migraciones y controlador

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index()
+    {
+        $blogs = $this->getDoctrine()
+            ->getRepository('App\Entity\Blog')
+            ->findAll();
+
+        return $this->render(
+            'blog/index.html.twig',
+            array('blogs' => $blogs)
+        );
+
+        /*return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);*/
+    }
+}
---
+++ src/Entity/Blog.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BlogRepository")
+ */
+class Blog
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+    */
+    private $topic;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+    */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+    */
+    private $body;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+    */
+    private $author;
+
+    /**
+     * @ORM\Column(type="text", name="url_image")
+    */
+    private $urlImage;
+    
+    /**
+     * @ORM\Column(type="datetime", name="created_at")
+    */
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of topic
+     */ 
+    public function getTopic()
+    {
+        return $this->topic;
+    }
+
+    /**
+     * Set the value of topic
+     *
+     * @return  self
+     */ 
+    public function setTopic($topic)
+    {
+        $this->topic = $topic;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of title
+     */ 
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @return  self
+     */ 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of body
+     */ 
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * Set the value of body
+     *
+     * @return  self
+     */ 
+    public function setBody($body)
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of author
+     */ 
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set the value of author
+     *
+     * @return  self
+     */ 
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of urlImage
+     */ 
+    public function getUrlImage()
+    {
+        return $this->urlImage;
+    }
+
+    /**
+     * Set the value of urlImage
+     *
+     * @return  self
+     */ 
+    public function setUrlImage($urlImage)
+    {
+        $this->urlImage = $urlImage;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of createdAt
+     */ 
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set the value of createdAt
+     *
+     * @return  self
+     */ 
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
