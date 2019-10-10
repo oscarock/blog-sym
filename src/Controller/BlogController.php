476d61f8406e200a03e751c6433d9d87bc80bb4f
Oscar Diaz <oscardiaz@lavenirapps.co>
2019-10-10 11:51:10 -0500

    Crea la funcionalidad de editar entrada blog

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Blog;
+use App\Form\BlogFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -29,21 +30,23 @@ class BlogController extends AbstractController
         );
     }
 
+    /**
+     * @Route("/new", name="new")
+    */
     public function new(Request $request)
     {
-        // creates a task object and initializes some data for this example
         $blogs = new Blog();
-        //$blogs->setTopic('Write a blog post');
-        //$blogs->setDueDate(new \DateTime('tomorrow'));
 
-        $form = $this->createFormBuilder($blogs)
+        /*$form = $this->createFormBuilder($blogs)
         ->add('topic', ChoiceType::class, ['choices' => ['Juegos' => 'juegos', 'Tecnologia' => 'tecnologia', 'Belleza' => 'belleza'], 'label' => 'Categoria', 'required' => 'required', 'attr' => ['class' => 'form-control', 'placeholder' => 'Categoria']])
         ->add('title', TextType::class, ['label' => 'Titulo', 'required' => 'required', 'attr' => ['class' => 'form-control', 'placeholder' => 'Titulo']])
         ->add('body', TextareaType::class, ['label' => 'Contenido', 'required' => 'required', 'attr' => ['class' => 'form-control', 'placeholder' => 'Contenido']])
         ->add('author', TextType::class, ['label' => 'Autor', 'required' => 'required', 'attr' => ['class' => 'form-control', 'placeholder' => 'Autor']])
         ->add('image', TextareaType::class, ['label' => 'Url Imagen', 'required' => 'required', 'attr' => ['class' => 'form-control', 'placeholder' => 'Url Imagen']])
         ->add('save', SubmitType::class, ['label' => 'Guardar', 'attr' => ['class' => 'btn btn-success form-control mt-3']])
-        ->getForm();
+        ->getForm();*/
+
+        $form = $this->createForm(BlogFormType::class, $blogs);
 
         $form->handleRequest($request);
 
@@ -66,7 +69,7 @@ class BlogController extends AbstractController
 
     /**
      * @Route("/show", name="show")
-     */
+    */
     public function show($id)
     {
         $blog = $this->getDoctrine()
@@ -75,7 +78,7 @@ class BlogController extends AbstractController
 
         if(!$blog){
             throw $this->createNotFoundException(
-                'No se encontro el id'. $id
+                'No se encontro el id'
             );
         }
 
@@ -84,4 +87,35 @@ class BlogController extends AbstractController
             array('blog' => $blog)
         );
     }
+
+    /**
+     * @Route("/edit", name="edit")
+    */
+    public function edit(Request $request, $id)
+    {
+        $do = $this->getDoctrine()->getManager();
+        $blog = $do->getRepository('App\Entity\Blog')->find($id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException(
+                'No se encontro el id'
+            );
+        }
+
+        $form = $this->createForm(BlogFormType::class, $blog);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $blog = $form->getData();
+            $do->flush();
+            return $this->redirect('/blog/');
+        }
+
+        return $this->render('blog/edit.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+
 }
